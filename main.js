025792531b097d98d1428f100dd35c59c0a40323
joicemonteiros <150451362+joicemonteiros@users.noreply.github.com>
2024-06-07 11:22:07 -0300

    first commi

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSom_Pom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
